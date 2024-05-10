bf2c55a93e43080fcea01041645f86009e0312f2
Krish Singh 01 <krishkumarsingh2003@gmail.com>
2024-05-10 17:40:27 +0530

    f

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:
+      {
+        roboto: ['Roboto slab'],
+        noto: ['Noto Sans Japanese'],
+        zilla: ['zilla slab'],
+        merriweather: ['Merriweather'],
+        merirobo: ['Merriweather, Roboto'],
+        open: ['Open Sans'],
+        roboto3: ['Roboto']
+      },
+    },
+  },
+  plugins: [],
+}
+
